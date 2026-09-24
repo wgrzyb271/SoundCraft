75ef9457ab33bdf81841b753bec74155fc18e39f
Emilia Morawska <emilia.morawska.06@gmail.com>
2026-09-24 02:49:40 +0200

    feat: allow for frontend connection

--- SoundCraft_backend/app/main.py
+++ SoundCraft_backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from .endpoints import upload_router, result_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""]
+)
 app.include_router(upload_router)
 app.include_router(result_router)
 #run:  uvicorn app.main:app --reload
