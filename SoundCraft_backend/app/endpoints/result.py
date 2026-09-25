0e07a6b893c4dc4f97367c334018f0ea607a5a0f
Emilia Morawska <emilia.morawska.06@gmail.com>
2026-09-25 23:42:29 +0200

    feat: seperate upload endpoint, add logic for retrieve latest agent response and changed audio

--- SoundCraft_backend/app/endpoints/result.py
+++ SoundCraft_backend/app/endpoints/result.py
@@ -1,57 +1,101 @@
 from fastapi import APIRouter,HTTPException
-from ..services import transfer_service
+from ..services import transfer_service, ResultType
 from pathlib import Path
 import json 
 from fastapi.responses import FileResponse
+from datetime import datetime,timezone
 
 result_router = APIRouter()
 
-@result_router.get("/result/{request_id}")
+def get_request_dir(request_id:str)->Path:
+    return Path(f"/tmp/agent_requests/{request_id}")
+
+def is_expired(request_id:str)->bool:
+    request_dir = get_request_dir(request_id)
+    metadata_files = list(request_dir.glob("prompt_*.json"))
+
+    if not metadata_files:
+        return False
+    
+    latest_metadata = max(metadata_files,key=lambda path: path.stat().st_mtime)
+    metadata = json.loads(latest_metadata.read_text(encoding="utf-8"))
+    expires_at = datetime.fromisoformat(metadata["expires_at"])
+    return datetime.now(timezone.utc) >= expires_at
+
+def get_latest_result(request_id:str, result_type:ResultType):
+    files = transfer_service.list_result_files(request_id,result_type)
+    if not files:
+        return None
+    latest_file = max(files, key=lambda file:file.st_mtime)
+    return latest_file.filename
+        
+@result_router.get("/result/{request_id}/agent_response")
 def get_result(request_id:str):
-    request_dir = Path(f"/tmp/agent_requests/{request_id}")
-    request_dir.mkdir(parents=True, exist_ok=True)
-
-    response_path = request_dir/"response.json"
-
-    try:
-        transfer_service.download_result(
-            request_id,
-            "response.json",
-            response_path
-        )
-    except Exception:
+    if is_expired(request_id):
         return {
-            "status":"processing",
+            "status":"expired",
             "request_id":request_id
         }
+    try:
+        latest_filename = get_latest_result(request_id,ResultType.AGENT_RESPONSE)
 
-    response = json.loads(
-        response_path.read_text(encoding="utf-8")
-    )
+        if latest_filename is None:
+            return{
+                "status": "processing",
+                "request_id": request_id
+            }
+        request_dir = get_request_dir(request_id)
+        response_path = (request_dir / "latest_response.json")
+        transfer_service.download_result(request_id, latest_filename, response_path, ResultType.AGENT_RESPONSE )
+
+    except Exception as e:
+        print (f"Could not get agent response: {e}") 
+        return{
+            "status": "processing",
+            "request_id": request_id
+            }
+
+    try:
+        response = json.loads(response_path.read_text(encoding="utf-8"))
+    except Exception as e:
+        print(f"Could not parse response: {e}")
+        raise HTTPException(status_code=500,detail="Invalid agent response")
     return{
-        "status":"completed",
-        "request_id":request_id,
-        "response":response
+        "status": "completed",
+        "request_id": request_id,
+        "filename": latest_filename,
+        "response": response
     }
 
 @result_router.get("/result/{request_id}/audio")
 def get_result_audio(request_id:str):
-    request_dir = Path(f"/tmp/agent_requests/{request_id}")
-    request_dir.mkdir(parents=True, exist_ok=True)
-
-    audio_path = request_dir / "audio.wav"
+    if is_expired(request_id):
+        raise HTTPException(status_code=410, detail="Request has expired")
 
     try:
+        latest_filename = get_latest_result(request_id, ResultType.AUDIO)
+
+        if latest_filename is None:
+            raise HTTPException(status_code=404, detail="Audio is not ready")
+
+        request_dir = get_request_dir(request_id)
+        audio_path = request_dir/latest_filename
+        
+        print(f"LATEST AUDIO: {latest_filename}")
+        print(f"LOCAL AUDIO PATH: {audio_path}")
+
         transfer_service.download_result(
             request_id,
-            "audio.wav",
-            audio_path
+            latest_filename,
+            audio_path,
+            ResultType.AUDIO
         )
-    except Exception:
-        raise HTTPException(
-            status_code=404,
-            detail="Audio is not ready"
-        )
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        print (f"Could not get audio:{e}")
+        raise HTTPException(status_code=404, detail="Audio is not ready")
 
     return FileResponse(
         audio_path,
@@ -62,9 +106,7 @@ def get_result_audio(request_id:str):
 @result_router.delete("/result/{request_id}")
 def delete_result(request_id: str):
     try:
-        transfer_service.delete_request(
-            request_id
-        )
+        transfer_service.delete_request(request_id)
 
         return {
             "status": "deleted",
@@ -72,7 +114,4 @@ def delete_result(request_id: str):
         }
 
     except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Could not delete request: {e}",
-        )
+        raise HTTPException(status_code=500,detail=f"Could not delete request: {e}")
--- SoundCraft_backend/app/endpoints/upload.py
+++ SoundCraft_backend/app/endpoints/upload.py
@@ -1,19 +1,14 @@
 from fastapi import APIRouter, File, Form, UploadFile, HTTPException
-from ..services import transfer_service, is_real_wav, convert_to_wav, save_upload
+from ..services import transfer_service, is_real_wav, convert_to_wav, save_upload, UploadType
 from uuid import uuid4
 from pathlib import Path
 import json 
-
+from datetime import datetime, timezone, timedelta
+from ..config import settings
 upload_router = APIRouter()
 
-@upload_router.post("/upload")
-async def upload(prompt:str=Form(...),audio:UploadFile = File(...)):
-
-    if not prompt.strip():
-        raise HTTPException(
-            status_code=400,
-            detail="Prompt cannot be empty",
-        )
+@upload_router.post("/upload/audio/")
+async def upload_audio(audio:UploadFile = File(...)):
 
     request_id = str(uuid4())
     request_dir = Path(f"/tmp/agent_requests/{request_id}")
@@ -26,30 +21,59 @@ async def upload(prompt:str=Form(...),audio:UploadFile = File(...)):
         await save_upload(audio, original_path)
 
         wav_path = request_dir/"audio.wav"
-
+        print(f"WAV_PATH: {wav_path}\n")
         if is_real_wav(original_path):
             original_path.rename(wav_path)
         else:
             convert_to_wav(original_path,wav_path)
 
-            metadata = {
-                "request_id":request_id,
-                "prompt":prompt,
-                "audio":"audio.wav"
-            }
-
-        metadata_path = request_dir/"metadata.json"
-        metadata_path.write_text(json.dumps(metadata,indent=2),encoding="utf-8")
-
-        transfer_service.transfer(wav_path, request_id)
-        transfer_service.transfer(metadata_path, request_id)
+        transfer_service.transfer(wav_path, request_id,UploadType.AUDIO)
     
         return {
             "status":"received",
             "request_id":request_id,
-            "prompt":prompt,
             "filename":audio.filename
         }
+    except Exception:
+        try:
+            transfer_service.delete_request(request_id)
+        except Exception:
+            pass
+        raise
+
+@upload_router.post("/upload/{request_id}/prompt")
+async def upload_prompt(request_id:str, prompt:str=Form(...)):
+    if not prompt.strip():
+        raise HTTPException(
+            status_code=400,
+            detail="Prompt cannot be empty",
+        )
+    try: 
+        request_dir = Path(f"/tmp/agent_requests/{request_id}")
+        request_dir.mkdir(parents=True,exist_ok=True)
+
+        now = datetime.now(timezone.utc)
+        expires_at = now + timedelta(seconds=settings.processing_ttl)
+        metadata={
+            "request_id":request_id,
+            "prompt":prompt,
+            "created_at":now.isoformat(),
+            "expires_at":expires_at.isoformat(),
+            "status":"processing"
+        }
+
+        timestamp = now.strftime("%Y%m%d_%H%M%S_%f")
+        prompt_path = request_dir/f"prompt_{timestamp}.json"
+        prompt_path.write_text(json.dumps(metadata,indent=2),encoding="utf-8")
+
+        print(f"METADATA_PATH: {prompt_path}\n")
+        transfer_service.transfer(prompt_path,request_id, UploadType.PROMPT)
+
+        return {
+                "status":"received",
+                "request_id":request_id,
+                "prompt":prompt
+            }
     except Exception:
         try:
             transfer_service.delete_request(request_id)
